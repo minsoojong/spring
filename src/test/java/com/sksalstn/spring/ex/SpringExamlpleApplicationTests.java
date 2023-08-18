6fdf469989bfebdf87e73ee01bbb49875bbe0ba9
403-15\user <minsoojong@naver.com>
2023-08-18 18:14:13 +0900

    [김민수]  - spring 예제

---
+++ src/test/java/com/sksalstn/spring/ex/SpringExamlpleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sksalstn.spring.ex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringExamlpleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
