429accc1c9e3c60b630f8d1e50a41392362283d6
403-15\user <minsoojong@naver.com>
2023-08-30 18:44:58 +0900

    [김민수]  - jstl 추가

--- src/main/java/com/sksalstn/spring/ex/user/UserController.java
+++ src/main/java/com/sksalstn/spring/ex/user/UserController.java
@@ -1,13 +1,21 @@
 package com.sksalstn.spring.ex.user;
 
+
+
 import org.springframework.stereotype.Controller;
 
-@Controller
-public class UserController {
-	
-	User user = new User();
-	user.setName(name);
-	user.setYyyymmdd(birthday);
-	
 
-}
+//@Controller
+//public class UserController {
+//	
+//	User user = new User();
+//	user.setName(name);
+//	user.setYyyymmdd(birthday);
+//	user.setEmail(email);
+//	
+//	int count = userService.addUserByObject(user);
+//	
+//	return "/jsp/userInfo";
+//	
+//
+//}
